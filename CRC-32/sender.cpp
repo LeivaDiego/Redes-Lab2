65baeec740981aa436e1678a53a2045c552043aa
Diego Leiva <110699607+LeivaDiego@users.noreply.github.com>
2024-07-25 22:38:45 +0000

    CRC-32 Encoding Algorithm
    - Removed the need to transform the polynom from hex to bin
    
    - Improved previous implementation, results as expected

--- CRC-32/sender.cpp
+++ CRC-32/sender.cpp
@@ -1,33 +1,19 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <math.h>
-#include <stdio.h>
-
 
 using namespace std;
 
 // Polinomio generador para CRC-32
-const uint32_t POLY = 0xEDB88320; // 1 0000 0100 1100 0001 0001 1101 1011 0111
+const string POLY = "100000100110000010001110110110111"; //  0xEDB88320
 
 
-// Función que convierte un número decimal a binario
-string polyToBinaryString(uint32_t poly) {
-    string binary;
-    while (poly > 0) {
-        binary = to_string(poly % 2) + binary;
-        poly /= 2;
-    }
-    binary += "1";
-    reverse(binary.begin(), binary.end()); // Invertir el string para obtener el número binario correcto
-    return binary;
-}
-
-// Función que aplica XOR entre dos números
-string xorOperation(string a, string b) {
+// Funcion que aplica XOR entre dos strings
+string binaryXor(string a, string b) {
     string result = "";
 
-    // Se realiza la operación XOR bit a bit
-    for (int i = 0; i < a.length(); i++) {
+    int n = b.length(); // Longitud de la cadena b
+
+    // Se realiza el XOR bit a bit
+    for (int i = 1; i < n; i++) {
         // Si los bits son iguales, se agrega un 0 al resultado
         if (a[i] == b[i]) {
             result += "0";
@@ -39,86 +25,86 @@ string xorOperation(string a, string b) {
     return result;
 }
 
-// Función que realiza la división de dos números binarios
-// retorna el residuo de la división
-string divide(string dividend, string divisor) {
-    int divisorLength = divisor.length();   // Longitud del divisor
-    int dividendLength = dividend.length(); // Longitud del dividendo
-    
-    int i = 0; // Contador
 
-    string result = ""; // Residuo de la división
-    string temp = dividend.substr(0, divisorLength); // Subcadena del dividendo
+// Funcion que realiza la division modulo 2
+string modulo2division(string dividend, string divisor) {
+    // Se obtiene la cantidad de bits a aplicar XOR a la vez
+    int pick = divisor.length();
 
-    // Se realiza la división
-    while (divisorLength < dividendLength) {
-        // Si el primer bit del dividendo es 0, se realiza un shift a la izquierda
-        if (temp[0] == '0') {
-            temp = temp.substr(1, temp.length()) + dividend[divisorLength];
-        // Si el primer bit del dividendo es 1, se realiza un XOR con el divisor
-        } else {
-            temp = xorOperation(temp, divisor) + dividend[divisorLength];
+    // Se divide el mensaje en partes de tamaño pick
+    string block = dividend.substr(0, pick);
+
+    // Se obtiene la longitud del mensaje
+    int n = dividend.length();
+
+    // Se realiza la division modulo 2
+    while (pick < n) {
+        // Si el primer bit del bloque es 1, 
+        // se reemplaza el bloque por el resultado de la operacion XOR
+        if (block[0] == '1') {
+            block = binaryXor(divisor, block) + dividend[pick];
         }
-        divisorLength++;
+        // Si el primer bit del bloque es 0,
+        // se usa un bloque de ceros para realizar la operacion XOR
+        else {
+            block = binaryXor(string(pick, '0'), block) + dividend[pick];
+        }
+        // Se incrementa el contador
+        pick++;
     }
 
-    // Se realiza la última operación XOR
-    if (temp[0] == '1') {
-        temp = xorOperation(temp, divisor);
+    // Para los ultimos bits del mensaje se realiza la operacion XOR
+    if (block[0] == '1') {
+        block = binaryXor(divisor, block);
+    } else {
+        block = binaryXor(string(pick, '0'), block);
     }
 
-    // Se obtiene el residuo de la división
-    result = temp.substr(1, temp.length());
-    return result;
+    return block;
 }
 
-// Función que realiza la codificación CRC-32
-string encodeCRC32(string message) {
-    string polynomial_binary = polyToBinaryString(POLY);    // Polinomio generador en binario
-    cout << "Polinomio generador: " << polynomial_binary << endl;
-    string dividend = message;                              // Dividendo
-    string divisor = polynomial_binary;                     // Divisor
-    int dividendLength = dividend.length();                 // Longitud del dividendo
 
-    // Se agrega ceros al dividendo
-    dividend.append(32, '0');
+// Funcion que realiza la codificacion CRC-32
+string encodeCRC32(string message, string polynomial) {
+    int n = polynomial.length(); // Longitud del polinomio generador
 
-    // Se invierten los primeros 32 bits el dividendo
-    for (int i = 0; i < 32; i++) {
-        if (dividend[i] == '1') {
-            dividend[i] = '0';
-        } else {
-            dividend[i] = '1';
-        }
-    }
+    // Se agrega n-1 ceros al final del mensaje
+    string augmented_message = message + string(n - 1, '0');
 
-    // Se realiza la división
-    string remainder = divide(dividend, divisor);
+    // Se realiza la division modulo 2 para obtener el residuo
+    string remainder = modulo2division(augmented_message, polynomial);
     cout << "Residuo: " << remainder << endl;
 
     // Se obtiene el mensaje codificado
-    string encodedMessage = message + remainder;
-    return encodedMessage;
+    string encoded_message = message + remainder;
+
+    // Se retorna el mensaje codificado y el residuo
+    return encoded_message;
 }
 
-// Funcion main 
+
+// Funcion principal
 int main() 
 {   
     string message; // Mensaje a enviar
     string encodedMessage; // Mensaje codificado
     
     cout << "Emisor CRC-32" << endl;
+    // Se muestra el polinomio generador
+    cout << "Polinomio generador: " << POLY << endl;
+    cout << endl;
 
     // Datos a enviar
     cout << "Ingrese los datos a enviar: ";
     cin >> message;
+    cout << endl;
 
     // Se realiza la codificación CRC-32
-    encodedMessage = encodeCRC32(message);
-
+    encodedMessage = encodeCRC32(message, POLY);
+    cout << endl;
+    
     // Se muestra el mensaje codificado
     cout << "Mensaje codificado: " << encodedMessage << endl;
-
 
 	return 0; 
 } 
